4034395cd63021d5b54ce532bcd9483257edaa97
Gefferson Vivan <geffersonvivan@MacBook-Air-de-Gefferson.local>
2026-04-06 22:23:40 -0300

    chore: setup files for Railway production deploy (Procfile, dependencies, whitenoise)

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,17 +1,17 @@
 from .base import *
 from decouple import config
+import dj_database_url
 
 DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app']
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default='postgres://localhost'),
+        conn_max_age=600
+    )
 }
 
 SECURE_HSTS_SECONDS = 31536000
